3806320e894a813370fd498ea0369abd9640cc09
Antoni Solarski <antsol1000@gmail.com>
2023-06-13 18:45:59 +0200

    reformat scripts

--- book_script.py
+++ book_script.py
@@ -1,54 +1,72 @@
-import PyPDF2
-import pandas as pd
 import os
 
+import PyPDF2
+import pandas as pd
 import spacy
-nlp = spacy.load('en_core_web_sm')
 
-book_name = 'ferdydurke'
+nlp = spacy.load("en_core_web_sm")
+
+book_name = "ferdydurke"
 cwd = os.getcwd()
 path = f"{cwd}/input/raw_books/{book_name}.pdf"
 end_path = f"{cwd}/input/processed_books/{book_name}.txt"
 df_path = f"{cwd}/input/processed_books/{book_name}.csv"
-encoding = 'utf-8'
-DEFAULT_CONTEXT_LENGTH = 8 # context + response length
+encoding = "utf-8"
+DEF_CONTEXT_LEN = 8  # context + response length
+
+
 def preprocess_pdf():
-    phrases_to_snip_out = ['Free eBooks at Planet eBoo k.com', "-"]
+    phrases_to_snip_out = ["Free eBooks at Planet eBoo k.com", "-"]
     text = """"""
-    pdfFileObj = open(path, 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    pdf_file_obj = open(path, "rb")
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
     # printing number of pages in pdf file
-    print("number of pages in document:", pdfReader.numPages)
-    
-    for page_number in range(2, pdfReader.numPages):
-        pageObj = pdfReader.getPage(page_number)
-        pageText =pageObj.extractText()
+    print("number of pages in document:", pdf_reader.numPages)
+
+    for page_number in range(2, pdf_reader.numPages):
+        page_obj = pdf_reader.getPage(page_number)
+        page_text = page_obj.extractText()
         for subtexts in phrases_to_snip_out:
-            pageText = pageText.replace(subtexts, ' ')
-        text += pageText    
-    pdfFileObj.close()
-    with open(end_path, 'w', encoding=encoding) as end_file:
+            page_text = page_text.replace(subtexts, " ")
+        text += page_text
+    pdf_file_obj.close()
+    with open(end_path, "w", encoding=encoding) as end_file:
         end_file.writelines(text)
-        
+
+
 def text_into_df():
-    with open(end_path, 'r', encoding=encoding) as end_file:
+    with open(end_path, "r", encoding=encoding) as end_file:
         text_corpora = end_file.read()
-        text = text_corpora.replace("\n", " ").replace("\t", " ").replace(';', '')
-    
+        text = (
+            text_corpora.replace("\n", " ").replace("\t", " ").replace(";", "")
+        )
+
     dos = nlp(text)
     text_sentences = [sent for sent in dos.sents]
     # prepare dictionnary
-    number_of_sentences = len(text_sentences)
-    contexts = [text_sentences[DEFAULT_CONTEXT_LENGTH + c:number_of_sentences - DEFAULT_CONTEXT_LENGTH + c] for c in range(DEFAULT_CONTEXT_LENGTH - 1)]
-    dictionnary_to_df = dict([(f"context{i}", context) for i, context in enumerate(contexts, start = 1)])
-    dictionnary_to_df["response"] = text_sentences[(DEFAULT_CONTEXT_LENGTH + 1): -(DEFAULT_CONTEXT_LENGTH-1)]
-    text_df = pd.DataFrame(dictionnary_to_df)
+    senten_len = len(text_sentences)
+    contexts = [
+        text_sentences[DEF_CONTEXT_LEN + c: senten_len - DEF_CONTEXT_LEN + c]
+        for c in range(DEF_CONTEXT_LEN - 1)
+    ]
+    dictionary_to_df = dict([
+        (f"context{i}", context)
+        for i, context in enumerate(contexts, start=1)
+    ])
+    dictionary_to_df["response"] = text_sentences[
+                                   (DEF_CONTEXT_LEN + 1): -(
+                                           DEF_CONTEXT_LEN - 1)
+                                   ]
+    text_df = pd.DataFrame(dictionary_to_df)
 
-    text_df.to_csv(df_path, index=False, encoding=encoding, sep = ';')
+    text_df.to_csv(df_path, index=False, encoding=encoding, sep=";")
+
 
 def main():
     preprocess_pdf()
     text_into_df()
     return 0
 
-main()
+
+if __name__ == '__main__':
+    main()
--- frontend/chatbot.py
+++ frontend/chatbot.py
@@ -11,9 +11,14 @@ class ChatBot:
         self.chat_history_ids = None
 
     def get_response(self, user_input: str) -> str:
-        new_user_input_ids = self.tokenizer.encode(user_input + self.tokenizer.eos_token, return_tensors="pt")
-        bot_input_ids = torch.cat(
-            [self.chat_history_ids, new_user_input_ids], dim=-1) if self.chat_history_ids else new_user_input_ids
+        new_user_input_ids = self.tokenizer.encode(
+            user_input + self.tokenizer.eos_token, return_tensors="pt"
+        )
+        bot_input_ids = (
+            torch.cat([self.chat_history_ids, new_user_input_ids], dim=-1)
+            if self.chat_history_ids
+            else new_user_input_ids
+        )
 
         chat_history_ids = self.model.generate(
             bot_input_ids,
@@ -26,7 +31,10 @@ class ChatBot:
             temperature=0.8,
         )
 
-        return self.tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+        return self.tokenizer.decode(
+            chat_history_ids[:, bot_input_ids.shape[-1]:][0],
+            skip_special_tokens=True,
+        )
 
 
 def init_streamlit():
@@ -47,7 +55,9 @@ def main():
     def callback():
         st.session_state.temp = st.session_state.input
         st.session_state.past.append(st.session_state.temp)
-        st.session_state.generated.append(chatbot.get_response(st.session_state.temp))
+        st.session_state.generated.append(
+            chatbot.get_response(st.session_state.temp)
+        )
         st.session_state.input = ""
 
     st.text_input("You:", "", key="input", on_change=callback)
@@ -55,7 +65,9 @@ def main():
     if st.session_state["generated"]:
         for i in range(len(st.session_state["generated"]) - 1, -1, -1):
             message(st.session_state["generated"][i], key=str(i))
-            message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
+            message(
+                st.session_state["past"][i], is_user=True, key=str(i) + "_user"
+            )
 
 
 if __name__ == "__main__":
