784a48eda9e96e15dff12e7d87035577f9248c5f
Oskar Szudzik <oszudzik@gmail.com>
2023-06-14 22:21:03 +0200

    classifier fix, metrics

--- chatbot/classifier.py
+++ chatbot/classifier.py
@@ -7,17 +7,15 @@ class Classifier:
         self.device = device
         self.model = RobertaForSequenceClassification.from_pretrained(
             model_path).to(self.device)
-        self.tokenizer = RobertaTokenizer.from_pretrained(model_path)
+        self.tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
 
     def get_input_tokens(self, user_input: str) -> torch.Tensor:
-        return self.tokenizer.encode(
-            user_input + self.tokenizer.eos_token, return_tensors="pt"
-            ).to(self.device)
+        return self.tokenizer(user_input, return_tensors="pt")
 
-    def get_response(self, user_input: str) -> int:
-        new_user_input_ids = self.get_input_tokens(user_input)
+    def get_prediction(self, user_input: str) -> int:
+        inputs = self.get_input_tokens(user_input)
         with torch.no_grad():
-            outputs = self.model(**new_user_input_ids)
+            outputs = self.model(**inputs)
             logits = outputs.logits
             probabilities = torch.softmax(logits, dim=-1)
             predicted_class = torch.argmax(probabilities, dim=-1)
