7a23d70c9557e666232a95194cb4802067615f69
Oskar Szudzik <oszudzik@gmail.com>
2023-06-14 21:14:19 +0200

    fix

--- src/classifier.py
+++ src/classifier.py
@@ -1,6 +1,5 @@
-import streamlit as st
 import torch
-from transformers import RobertaTokenizer, RobertaForSequenceClassification 
+from transformers import RobertaTokenizer, RobertaForSequenceClassification
 
 
 class Classifier:
